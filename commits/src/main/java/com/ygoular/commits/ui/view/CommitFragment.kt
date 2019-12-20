9790261e203e28d22c9cca394bcc47abe8f12306
Yoann Goular <ygoular@gmail.com>
2019-12-20 23:40:05 +0100

    Create an adapter for commits
    Commits information are now displayed in a
    dedicated adapter. This includes a refresh
    feature.

--- commits/src/main/java/com/ygoular/commits/ui/view/CommitFragment.kt
+++ commits/src/main/java/com/ygoular/commits/ui/view/CommitFragment.kt
@@ -1,18 +1,25 @@
 package com.ygoular.commits.ui.view
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.ygoular.commits.R
+import com.ygoular.commits.ui.adapter.CommitAdapter
 import com.ygoular.commits.viewmodel.CommitViewModel
 import kotlinx.android.synthetic.main.commit_fragment.*
 
 class CommitFragment : Fragment() {
 
     private val mViewModel: CommitViewModel by viewModels()
+    private val mCommitAdapter = CommitAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,14 +30,61 @@ class CommitFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fetchCommits()
+
+        recycler_view_commits.layoutManager = LinearLayoutManager(context)
+        recycler_view_commits.setHasFixedSize(true)
+        recycler_view_commits.adapter = mCommitAdapter
+
+        layout_refresh_commits.setOnRefreshListener { fetchCommits() }
+
+        refreshData()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.commits_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_update -> refreshData()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun fetchCommits() {
         mViewModel.getCommitList(
             "torvalds/linux",
-            onSuccess = { list -> text_default_message.text = list[0].toString()},
-            onError = { error -> text_default_message.text = error.mMessage })
+            onSuccess = { list ->
+                mCommitAdapter.submitList(list)
+                adaptUi()
+            },
+            onError = {
+                with(
+                    Snackbar.make(
+                        layout_commits,
+                        "${getString(R.string.error_retrieving_commits)} from ${it.mParam} repository",
+                        Snackbar.LENGTH_INDEFINITE
+                    )
+                ) {
+                    setAction("OK") { this.dismiss() }
+                    show()
+                }
+                adaptUi(it.mMessage)
+            })
+    }
+
+    private fun adaptUi(error: String = "") {
+        text_error_retrieving_commits.text = error
+        text_error_retrieving_commits.visibility =
+            if (error.isNotEmpty()) View.VISIBLE else View.GONE
+        recycler_view_commits.visibility = if (error.isNotEmpty()) View.GONE else View.VISIBLE
+        layout_refresh_commits.isRefreshing = false
+    }
+
+    private fun refreshData() {
+        layout_refresh_commits.isRefreshing = true
+        fetchCommits()
     }
 
 }
